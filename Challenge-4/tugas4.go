08212b8bccdae6c6a293e425467cf2cc691f8a74
PuppyGunawan <puppygunawan94@gmail.com>
2024-09-14 23:40:53 +0700

    Soal 2 Done

--- Challenge-4/tugas4.go
+++ Challenge-4/tugas4.go
@@ -5,6 +5,7 @@ import (
 )
 
 func main() {
+	//! Soal 1
 	num := 20
 	for i := 1; i <= num; i++ {
 		if i%3 == 0 && i%2 == 1 {
@@ -15,4 +16,16 @@ func main() {
 			fmt.Println(i, `- Santai`)
 		}
 	}
+	//!Soal 2
+	for i := 1; i <= 7; i++ {
+		for j := 1; j <= i; j++ {
+			//print # di tiap baris
+			fmt.Print("#")
+		}
+		//mencetak baris baru setelah print pagar
+		fmt.Println()
+	}
+	//!Soal 3
+	var kalimat = [...]string{"aku","dan","saya","sangat","senang","belajar","golang"}
+	
 }
