bcc19e105c8d2e7d65547cac672cb03ac8b7f3a3
PuppyGunawan <puppygunawan94@gmail.com>
2024-09-17 01:17:13 +0700

    Done Challenge 7 no 4

--- Challenge-7/tugas7.go
+++ Challenge-7/tugas7.go
@@ -35,27 +35,51 @@ type persegiPanjang struct {
 	panjang, lebar int
 }
 
-func(a segitiga) luasSegitiga() {
+func (a segitiga) luasSegitiga() {
 	luasTiga := a.alas * a.tinggi / 2
 	fmt.Println(luasTiga)
 }
-func(b persegi) luasPersegi(){
+func (b persegi) luasPersegi() {
 	luasPersegis := b.sisi * b.sisi
 	fmt.Println(luasPersegis)
 }
-func(c persegiPanjang) luasPersegiPanjang(){
+func (c persegiPanjang) luasPersegiPanjang() {
 	luasPersegiPanjangs := c.panjang * c.lebar
 	fmt.Println(luasPersegiPanjangs)
 }
-//!Soal 3
-type phone struct{
+
+// !Soal 3
+type phone struct {
 	name, brand string
-	year int
-	colors []string
+	year        int
+	colors      []string
 }
-//menggunakan pointer supaya setelah ditambahkan objek asli juga terganti valuenya
-func (p *phone) addColor(newColor string){
+
+// menggunakan pointer supaya setelah ditambahkan objek asli juga terganti valuenya
+func (p *phone) addColor(newColor string) {
 	p.colors = append(p.colors, newColor)
+}
+
+// !Soal 4
+type movie struct {
+	title, genre   string
+	duration, year int
+}
+
+func tambahDataFilm(title string, genre string, duration int, year int, dataFilm *[]movie) {
+	newMovie := movie{
+		title:    title,
+		duration: duration,
+		genre:    genre,
+		year:     year,
+	}
+	*dataFilm = append(*dataFilm, newMovie)
+}
+
+func showDataFilm(dataFilm []movie) {
+	for i, value := range dataFilm {
+		fmt.Printf("%d. title:%s\n duration:%d menit\n genre:%s\n year:%d\n\n", i+1, value.title, value.duration, value.genre, value.year)
+	}
 }
 
 func main() {
@@ -68,21 +92,28 @@ func main() {
 	}
 	printBuah(buahs)
 	//!Soal 2
-	var alas = segitiga{4,6}
+	var alas = segitiga{4, 6}
 	alas.luasSegitiga()
 	var sisi = persegi{4}
 	sisi.luasPersegi()
-	var pl = persegiPanjang{3,4}
+	var pl = persegiPanjang{3, 4}
 	pl.luasPersegiPanjang()
 	//!Soal 3
 	myPhone := phone{
-		name: "Iphone X",
+		name:  "Iphone X",
 		brand: "Apple",
-		year: 2019,
+		year:  2019,
 		colors: []string{
-			"Gold","Red",
+			"Gold", "Red",
 		},
 	}
 	myPhone.addColor("Blue")
 	fmt.Println(myPhone)
+	//!Soal 4
+	var dataFilm = []movie{}
+	tambahDataFilm("LOTR", "action", 120, 1999, &dataFilm)
+	tambahDataFilm("avenger", "action", 120, 2019, &dataFilm)
+	tambahDataFilm("spiderman", "action", 120, 2004, &dataFilm)
+	tambahDataFilm("juon", "horror", 120, 2004, &dataFilm)
+	showDataFilm(dataFilm)
 }
