42e3081f1fa10ee0362f7b300a4a1d347d89a3f7
PuppyGunawan <puppygunawan94@gmail.com>
2024-09-24 15:20:09 +0700

    Nomor 2 Done

--- Challenge-11/tugas11.go
+++ Challenge-11/tugas11.go
@@ -4,14 +4,21 @@ import (
 	"fmt"
 	"sort"
 	"sync"
-	"time"
+	// "time"
 )
 
 func showPhone(i int, value string, wg *sync.WaitGroup){
 	defer wg.Done()
-	time.Sleep(1 * time.Second)
+	// time.Sleep(1 * time.Second)
 	output:= fmt.Sprintf("%d. %s", i+1, value)
 	fmt.Println(output)
+}
+
+func getMovies(moviesChannel chan string,movies ...string){
+	for i, value := range movies {
+		moviesChannel <- fmt.Sprintf("%d. %s", i+1,value)
+	}
+	close(moviesChannel)
 }
 
 func main() {
@@ -25,5 +32,14 @@ func main() {
 		wg.Add(1)
 		go showPhone(i,value,&wg)
 		wg.Wait()
+	}
+	//!Soal 2
+	var movies = []string{"Harry Potter","LOTR", "SpiderMan", "Logan", "Avengers", "Insidious", "Toy Story"}
+	moviesChannel := make(chan string)
+
+	go getMovies(moviesChannel,movies...)
+	fmt.Println("List Movies:")
+	for value := range moviesChannel{
+		fmt.Println(value)
 	}
 }
